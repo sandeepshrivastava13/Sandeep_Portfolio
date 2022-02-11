e9d223bc6166246336fd6bb8e48e32890d48e32c
sandeepshrivastava13 <shrivastavasandeep13@gmail.com>
2022-02-11 17:15:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// const express = require('express');
+
+// // create new express app and save it as "app"
+// const app = express();
+// const path = require('path');
+// app.use(express.static(__dirname + '/public/index.css'));
+
+// // server configuration
+// const PORT = 8080;
+
+// // create a route for the app
+// app.get('/', (req, res) => {
+// 	res.sendFile(path.join(__dirname + '/home.html'));
+// });
+
+// // make the server listen to requests
+// app.listen(PORT, () => {
+// 	console.log(`Server running at: http://localhost:${PORT}/`);
+});
